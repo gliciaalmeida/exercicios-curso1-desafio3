8d4825482b403e670bcafa6a1d0d89b699166d4b
gliciaalmeida <121256693+gliciaalmeida@users.noreply.github.com>
2024-01-31 15:41:37 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+let contador = 0;
+while (contador < 11) {
+    console.log(contador);
+    contador = contador + 1;
+}
+
+let contadorNeg = 10;
+while (contadorNeg >= 0) {
+    console.log(contadorNeg);
+    contadorNeg--;
+}
+while (contagorNeg = 0) {
+    break;
+}
+
+let contagem = prompt('Por favor, digite um número');
+while (contagem != 0) {
+    console.log(contagem);
+    contagem--;
+}
+
+contagem2 = 0;
+let progressivo = prompt('Por favor, digite um outro número');
+while (progressivo != contagem2) {
+    console.log(contagem2);
+    contagem2++;
+}
